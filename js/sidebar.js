217389cad42d00398ebc443dc0fbc35162e63024
OlgaTikhonchik <olga882011@gmail.com>
2024-12-01 13:04:48 +0200

    Добавление кнопок

--- js/sidebar.js
+++ js/sidebar.js
@@ -9,3 +9,16 @@ document.addEventListener('DOMContentLoaded', function () {
       button.classList.toggle('active', !isActive);
     });
   });
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const button = document.querySelector('.menu-review-desk'); 
+    const list = document.querySelector('.menu-review'); 
+  
+   
+    button.addEventListener('click', function () {
+      const isActive = list.classList.contains('active');
+     list.classList.toggle('active', !isActive);
+      button.classList.toggle('active', !isActive);
+    });
+  });
